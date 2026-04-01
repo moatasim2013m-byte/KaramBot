710a43bf260c966e2100c24b962369ce697112f8
MoatasimShakhtoori <moatasim2013m@gmail.com>
2026-04-01 09:51:10 +0300

    Update app/page.tsx to implement a new responsive homepage

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// Updated Home Page
+import HeroSection from './components/HeroSection';
+import WorldCard from './components/WorldCard';
+
+const HomePage = () => {
+    return (
+        <div>
+            <HeroSection />
+            <WorldCard />
+        </div>
+    );
+};
+
+export default HomePage;
